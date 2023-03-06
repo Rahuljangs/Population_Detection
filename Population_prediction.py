3f94c1e5e301ad7e1f5fde46399bf9d5000cffae
Rahuljangs <87065976+Rahuljangs@users.noreply.github.com>
2023-03-06 12:36:22 +0530

    Update Population_prediction.py

--- Population_prediction.py
+++ Population_prediction.py
@@ -1,4 +1,4 @@
-# importing packages
+# importing necessary packages
 
 import pandas as pd
 import numpy as np
@@ -8,46 +8,49 @@ import json
 import warnings
 warnings.filterwarnings("ignore")
 
-# Loading the data from local machine, the location of data should be modified accordingly 
 
-df=data.iloc[:,:12]
-df=data.loc[df['Name']=='India']
-df.drop(['CCA3','Area (km²)','Density (per km²)','GrowthRate','World Population Percentage','Rank'],axis=1,inplace=True)
-df=df.T
-
-df=df.reset_index().rename(columns={1:'population','index':'year'})
-df=df.iloc[1:,:]
-
-# Creating a function to call LR that predicts the population of specified country with different models
+# Creating a function to call LR to reuse it multiple times
 
 def LR():
-    country = input("Please input the country name: ")
+    country = input("Please input the country name: ")              
     year = int(input("Please input the year to predict: "))
-    df = pd.read_csv('population.csv')
+    
+    #loading population dataset
+    df = pd.read_csv('population.csv')   
+    
+    #filtering only country names from the dataset
     lists=df.iloc[:,:15]
     lists.drop(['CCA3','Area (km²)','Density (per km²)','GrowthRate','World Population Percentage','Rank'],axis=1,inplace=True)
     lists=lists.T
     temp=lists.iloc[0,:]
     l=[i for i in temp]
+    
+    #checking if the given country name is present in the dataset or not
     if country in l:
       df = pd.read_csv('population.csv')
+    
+      #dropping unnecessary data from the dataset that are irrelevant  
       df=data.iloc[:,:12]
       df=data.loc[df['Name']==country]
       df.drop(['CCA3','Area (km²)','Density (per km²)','GrowthRate','World Population Percentage','Rank'],axis=1,inplace=True)
       df=df.T
-  
       df=df.reset_index().rename(columns={1:'population','index':'year'}) 
       df=df.iloc[1:,:]
-
+      
+      #dividing the datasets into x and y, ie : x is independent and y is dependent
       x = df.iloc[:, 0].values.reshape(-1,1)
       y = df.iloc[:, 1].values.reshape(-1,1)
+        
+      #training the dataset
       model = LinearRegression().fit(x,y)
+        
+      #retrieving the output
       print("Estimated population using Linear Regression model is :"+str(int(model.coef_[0][0] * year + model.intercept_[0])))
         
     else:
-        print('kindly check country name spelling\n')
+        print('kindly check the name or the country data isnt available \n')
         
-  # Calling LR() function to enter input data 
+  # Calling LR() function to take the input and print the output
   
   LR()
   
